3c1aa985b0b2dd5803d933ca4c9b71108a8f141b
hannatrap <107367091+hannatrap@users.noreply.github.com>
2022-07-04 11:51:57 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+console.log("Let's get this party started!");
+//searching within the API for a gif
+
+const pageContent = $('#page-content');
+const searchInput = $('#search');
+
+function addGif(res) {
+    let results = res.data.length;
+    if(results) {
+        let randomIdx = Math.floor(Math.random() * results);
+        let newDiv = $("<div>");
+        let newGif = $("<img>", {
+            src: res.data[randomIdx].images.original.url
+        });
+        newDiv.append(newGif);
+        pageContent.append(newDiv);
+
+    }
+}
+
+
+$("form").on("submit", async function(e) {
+e.preventDefault();
+
+let search = searchInput.val();
+searchInput.val("");
+
+    const response = await axios.get("http://api.giphy.com/v1/gifs/search", {params: {
+    q: search,
+    api_key: 'rd7GEruDqnqjGDaKfDMQavL7gzLAeaPz'
+}
+});
+addGif(response.data);
+});
+
+$("#delete").on("click", function() {
+    pageContent.empty();
+});
+
+
+
+//adding gif to page
